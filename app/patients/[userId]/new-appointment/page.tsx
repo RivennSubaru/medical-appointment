2486205065885d7a8db3a9605c925d0f65692e88
Rivenn <zafimenasteven@gmail.com>
2025-03-16 01:20:33 +0300

    feat: create AppointmentForm component and update NewAppointment layout

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import AppointmentForm from "@/components/AppointmentForm";
+import PatientForm from "@/components/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
-const NewAppointment = () => {
+export default function NewAppointment() {
   return (
-    <div>NewAppointment</div>
-  )
-}
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <span className="mb-12 h-10 w-fit">Logo</span>
 
-export default NewAppointment
+          <AppointmentForm/>
+
+          <p className="text-dark-600 xl:text-left justify-items-end">
+            © copyright
+          </p>
+        </div>
+      </section>
+
+      <Image 
+        src="/assets/images/appointment-img.png" 
+        alt="appointment" 
+        width={1000} height={1000} 
+        className="side-img w-[390px] bg-bottom rounded-s-2xl"
+      />
+    </div>
+  );
+}
